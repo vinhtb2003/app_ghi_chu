0cad249162f3c7bebe552cd8a619c921e3ccade8
vinhtb2003 <125486082+vinhtb2003@users.noreply.github.com>
2024-06-09 15:22:50 +0700

    lưu thông tin người dùng mới vào Firestore, sau đó điều hướng người dùng đến màn hình đăng nhập

--- lib/service/signUp_Service.dart
+++ lib/service/signUp_Service.dart
@@ -4,7 +4,7 @@
 import 'package:app_ghi_chu/views/login_Screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:get/get.dart';
+import 'package:get/get.dart';
 
 signUpUser(String userName, String userPhone, String userEmail,
     String userPassword) async {
