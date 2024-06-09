e4f628f6434cc7703b56112f7d37d892d0524095
vinhtb2003 <125486082+vinhtb2003@users.noreply.github.com>
2024-06-09 14:50:58 +0700

    Update createNote_Screen.dart

--- app_ghi_chu/lib/views/createNote_Screen.dart
+++ app_ghi_chu/lib/views/createNote_Screen.dart
@@ -2,7 +2,7 @@ import 'package:app_ghi_chu/views/home_Screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+// import 'package:get/get.dart';
 
 class CreateNoteScreen extends StatefulWidget {
   const CreateNoteScreen({super.key});
